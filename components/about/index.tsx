e233b0bb5c729aa10dd053a07297a2868dbf2ef2
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-10-24 10:21:51 +0200

    add id to about section

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from "next-intl";
 export default function About() {
   const t = useTranslations("about");
   return (
-    <section className="max-w-[600px]">
+    <section className="mb-12 max-w-[600px]" id="about">
       <h2 className="text-2xl font-bold">{t("title")}</h2>
       <p>{t("body")}</p>
     </section>
