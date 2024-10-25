3a2947d4db287512ba27b9e896a80e679669970c
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-10-25 14:21:21 +0200

    add tracing beam scroll animation

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,19 @@ import Experience from "@/components/experience";
 import Education from "@/components/education";
 import Projects from "@/components/projects";
 import { Separator } from "@/components/ui/separator";
+import { TracingBeam } from "@/components/ui/TracingBeam";
 
 export default function Home() {
   return (
     <>
-      <Profile />
-      <Separator className="mb-12 mt-6" />
-      <About />
-      <Experience />
-      <Education />
-      <Projects />
+      <TracingBeam>
+        <Profile />
+        <Separator className="mb-12 mt-6" />
+        <About />
+        <Experience />
+        <Education />
+        <Projects />
+      </TracingBeam>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,7 @@ const config: Config = {
   },
   plugins: [
     require("tailwindcss-animate"),
+    addVariablesForColors,
     function ({ matchUtilities, theme }: any) {
       matchUtilities(
         {
@@ -106,4 +107,17 @@ const config: Config = {
     },
   ],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
