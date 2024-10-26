7b7f790694fd9e87ef0e952df9ae1cbb115c74c8
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-10-26 15:23:18 +0200

    decrease spacing on mobile

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -30,13 +30,13 @@ export default function Projects() {
         )}
       </div>
       {projects.find((project) => project.devMode) && (
-        <Separator className="relative my-32">
-          <span className="absolute left-[18%] top-1/2 -translate-x-1/2 -translate-y-1/2 bg-background px-2 text-xl font-bold">
+        <Separator className="relative mb-16 mt-32 max-sm:mb-10 max-sm:mt-20">
+          <span className="absolute left-[18%] top-1/2 -translate-x-1/2 -translate-y-1/2 bg-background px-2 text-xl font-bold text-primary max-sm:left-[25%] max-sm:text-lg">
             In development
           </span>
         </Separator>
       )}
-      <div className="flex flex-col gap-24">
+      <div className="flex flex-col gap-24 max-sm:gap-12">
         {projects.map(
           (project, index) =>
             project.devMode && (
